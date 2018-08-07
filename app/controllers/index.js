15a150afc316d7929028992a387fa510421f0307
Robert <seedraev@gmail.com>
2018-08-07 09:58:59 +0200

    add response message

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -3,7 +3,18 @@ import { match, not } from "@ember/object/computed";
 
 export default Controller.extend({
   emailAddress: "",
+  responseMessage: "",
 
   isValid: match("emailAddress", /^.+@.+\..+$/),
-  isDisabled: not("isValid")
+  isDisabled: not("isValid"),
+
+  actions: {
+    saveInvitation() {
+      this.set(
+        "responseMessage",
+        `Thank you for your email address: ${this.get("emailAddress")}.`
+      );
+      this.set("emailAddress", "");
+    }
+  }
 });
