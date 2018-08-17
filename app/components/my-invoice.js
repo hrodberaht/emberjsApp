37181d426239b2926e4c9a6389776c3c026c4b2a
Robert <seedraev@gmail.com>
2018-08-17 18:51:25 +0200

    add showdetails to invoices

--- app/components/my-invoice.js
+++ app/components/my-invoice.js
@@ -1,4 +1,11 @@
-import Component from '@ember/component';
+import Component from "@ember/component";
 
 export default Component.extend({
+  showDetails: false,
+
+  actions: {
+    onDetails() {
+      this.toggleProperty("showDetails");
+    }
+  }
 });
