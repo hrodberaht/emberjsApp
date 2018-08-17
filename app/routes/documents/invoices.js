cf5322c81583f28a6ff7eeedb0602d4586b6bafc
Robert <seedraev@gmail.com>
2018-08-17 18:04:25 +0200

    add component my-inoice, model with sample invoice

--- app/routes/documents/invoices.js
+++ app/routes/documents/invoices.js
@@ -1,4 +1,42 @@
-import Route from '@ember/routing/route';
+import Route from "@ember/routing/route";
 
 export default Route.extend({
+  model() {
+    return [
+      {
+        number: "1234",
+        companyName: "Emtex",
+        products: [
+          {
+            name: "toy",
+            ean: "55555",
+            quantity: 2
+          },
+
+          {
+            name: "doll",
+            ean: "33333",
+            quantity: 1
+          }
+        ]
+      },
+      {
+        number: "1234",
+        companyName: "Emtex",
+        products: [
+          {
+            name: "toy",
+            ean: "55555",
+            quantity: 2
+          },
+
+          {
+            name: "doll",
+            ean: "33333",
+            quantity: 1
+          }
+        ]
+      }
+    ];
+  }
 });
--- app/routes/products.js
+++ app/routes/products.js
@@ -1,7 +1,3 @@
 import Route from "@ember/routing/route";
 
-export default Route.extend({
-  model() {
-    return this.get("store").findAll("product");
-  }
-});
+export default Route.extend({});
